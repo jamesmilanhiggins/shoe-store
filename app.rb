f2027d2a644bfa01a4b93d432c725800356a9d85
James Higgins <james.milan.higgins@gmail.com>
2017-05-12 09:13:18 -0700

    Add routing to add and list store

--- app.rb
+++ app.rb
@@ -5,3 +5,14 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 get "/" do
   erb :index
 end
+
+get "/stores" do
+  @sorted_stores = Store.all
+  erb :stores
+end
+
+post "/stores" do
+  name = params["name"]
+  Store.create({name: name})
+  redirect "/stores"
+end
--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -6,5 +6,4 @@ describe(Brand) do
     brand = Brand.new({name: "" })
     expect(brand.save).to eq false
   end
-
 end
